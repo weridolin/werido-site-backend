583f3ae6ddf989af7f408e1d7fd8facd258a099d
werido <359066432@qq.com>
2022-05-08 12:22:15 +0800

    📃 docs: update

--- core/asgi.py
+++ core/asgi.py
@@ -31,6 +31,6 @@ django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter,URLRouter
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": URLRouter(channel_router),
 })
