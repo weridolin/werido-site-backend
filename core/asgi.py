a0f69102fa100d36dff438abda6038c768c19194
werido <359066432@qq.com>
2022-05-08 11:17:43 +0800

    ✨ feat: update

--- core/asgi.py
+++ core/asgi.py
@@ -17,13 +17,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-from email.mime import application
+
 import os
 from django.core.asgi import get_asgi_application
 from ws.routing import channel_router
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# application = get_asgi_application()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 
 from channels.routing import ProtocolTypeRouter,URLRouter
 
