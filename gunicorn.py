9387ef5b9640cc4480f47399d1b877035052a1d2
werido <359066432@qq.com>
2022-05-08 16:00:11 +0800

    ✨ feat: UPDATE

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,8 @@
 # gunicorn.conf
+import multiprocessing
 
 # 并行工作进程数
-workers = 4
+workers = multiprocessing.cpu_count() * 2 + 1
 # 指定每个工作者的线程数
 threads = 2
 # 监听内网端口5000
@@ -9,7 +10,8 @@ bind = '0.0.0.0:8000'
 # 设置守护进程,将进程交给supervisor管理
 daemon = 'false'
 # 工作模式协程
-worker_class = 'gevent'
+# worker_class = 'gevent' # 只支持WSGI
+worker_class = 'uvicorn.workers.UvicornWorker' # 支持ASGI
 # 设置最大并发量
 worker_connections = 10000
 # 设置进程文件目录
