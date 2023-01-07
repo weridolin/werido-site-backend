764c153c9f205013a9527e5b9e0db8d61b6213e5
werido <359066432@qq.com>
2023-01-07 15:49:24 +0800

    ✨ feat: update

--- wechat/v1/apis.py
+++ wechat/v1/apis.py
@@ -4,7 +4,10 @@ from wechat.models import WechatMessage
 from wechat.v1.seriazliers import WechatMessageSerializer
 from rest_framework_xml.parsers import XMLParser
 from rest_framework_xml.renderers import XMLRenderer
-
+import hashlib
+import os
+from utils.http_ import HTTPResponse
+from rest_framework import status
 
 class PublicCountMessageApis(ModelViewSet):
 
@@ -12,8 +15,38 @@ class PublicCountMessageApis(ModelViewSet):
     parser_classes=[XMLParser]
     serializer_class =WechatMessageSerializer
     renderer_classes=[XMLRenderer]
-
+    queryset = WechatMessage
 
     def create(self, request, *args, **kwargs):
-        print(">>> get message",request.data)
+        print(">>> get wechat message",request.data)
         return super().create(request, *args, **kwargs)
+    
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            data = request.data
+            signature = data.get("signature")
+            timestamp = data.get("timestamp")
+            nonce = data.get("nonce")
+            echostr = data.get("echostr")
+            token = os.environ.get("WECHAT_URL_TOKEN")
+
+            list_ = [token, timestamp, nonce]
+            list_.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list_)
+            hashcode = sha1.hexdigest()
+
+            if hashcode == signature:
+                return HTTPResponse(
+                    message=echostr
+                )
+            else:
+                return HTTPResponse(
+                    status=status.HTTP_400_BAD_REQUEST,
+                    message=f"hashcode不等于signature"
+                )
+        except Exception as exc:
+            return HTTPResponse(
+                status=status.HTTP_400_BAD_REQUEST,
+                message=str(exc)
+            )
--- wechat/v1/urls.py
+++ wechat/v1/urls.py
@@ -2,7 +2,56 @@ from rest_framework import routers
 from django.urls import path,re_path
 from wechat.v1.apis import PublicCountMessageApis
 
-router = routers.SimpleRouter(trailing_slash=True)
+
+
+class WeChatApisRouter(routers.SimpleRouter):
+    ## 自定义下路由，把 delete 单个object改成 删除多个
+    routes = [
+        # List route.
+        routers.Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'retrieve',
+                # 'get': 'list',
+                'post': 'create',
+                'delete': 'destroy'
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        # Dynamically generated list routes. Generated using
+        # @action(detail=False) decorator on methods of the viewset.
+        routers.DynamicRoute(
+            url=r'^{prefix}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=False,
+            initkwargs={}
+        ),
+        # Detail route.
+        routers.Route(
+            url=r'^{prefix}/{lookup}{trailing_slash}$',
+            mapping={
+                # 'get': 'retrieve',
+                'put': 'update',
+                'patch': 'partial_update',
+                # 'delete': 'destroy'
+            },
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Instance'}
+        ),
+        # Dynamically generated detail routes. Generated using
+        # @action(detail=True) decorator on methods of the viewset.
+        routers.DynamicRoute(
+            url=r'^{prefix}/{lookup}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=True,
+            initkwargs={}
+        ),
+    ]  
+
+router = WeChatApisRouter(trailing_slash=True)
 router.register("public/message",viewset=PublicCountMessageApis,basename="wechat-public-count-message")
 # router.register("perms",viewset=PermissionApis,basename="rbac-perms")
 urlpatterns = [
