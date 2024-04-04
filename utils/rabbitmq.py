fbb5678d9bd0b1718e5ec4ae065d55fce9b93892
werido <359066432@qq.com>
2024-04-04 21:36:31 +0800

    feat:update

--- utils/rabbitmq.py
+++ utils/rabbitmq.py
@@ -1,28 +1,26 @@
 import pika
 from pika import DeliveryMode
 from pika.exchange_type import ExchangeType
+import os
 
-__instance = None
 
 def get_client():
-    global __instance
-    if not __instance:
-        credentials = pika.PlainCredentials('werido', '359066432')
-        parameters = pika.ConnectionParameters('www.weridolin.cn',
-            30003, 
-            credentials=credentials,
-            virtual_host='/',
-            heartbeat=2,)
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        __instance = channel
-    return __instance
+    credentials = pika.PlainCredentials(os.environ.get('RABBITMQ_DEFAULT_USER','werido'), os.environ.get('RABBITMQ_DEFAULT_PASS','359066432'))
+    parameters = pika.ConnectionParameters(os.environ.get("RABBITMQ_SVC_NAME",'www.weridolin.cn'),
+        os.environ.get("RABBITMQ_PORT",30003), 
+        credentials=credentials,
+        virtual_host='/',
+        heartbeat=2,  # 设置心跳周期，防止网络故障导致的假死
+        blocked_connection_timeout=300,  # 当连接被服务器阻塞时的超时时间
+        connection_attempts=3,  # 连接失败后的重试次数
+        retry_delay=5,  # 两次连接尝试之间的延迟
+        socket_timeout=10,  # 套接字操作超时时间
+    )
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    return channel
 
 def public_message(exchange, routing_key, message):
-    # credentials = pika.PlainCredentials('werido', '359066432')
-    # parameters = pika.ConnectionParameters('www.weridolin.cn',30003, credentials=credentials)
-    # connection = pika.BlockingConnection(parameters)
-    # client = connection.channel()
     client = get_client()
     client.exchange_declare(exchange=exchange,
         exchange_type=ExchangeType.topic,
