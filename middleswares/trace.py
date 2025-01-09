9ffc1ab0a3c90c06fa4684c6910cd045127cf840
werido <359066432@qq.com>
2025-01-09 09:55:11 +0800

    feat:update

--- middleswares/trace.py
+++ middleswares/trace.py
@@ -54,6 +54,7 @@ class OpenTracingMiddleware(MiddlewareMixin):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         headers = format_request_headers(request.META)
+        print(">>>>>>>>>>>>",headers)
         ctx = TraceContextTextMapPropagator().extract(headers) # 生成上下文
         self.span = self.tracer.start_span(name="svc-oldbackend", context=ctx) # 开启记录一个新的span
         self.token = context_api.attach(ctx)
